49dedb1b141209819c8e2ae2d89ee51cc0f0186e
RIZKIPUJI-SAMS\Rizki Puji Lestari <rizki@ekuator.id>
2023-12-14 01:55:09 +0700

    fix visi dan misi

--- resources/views/visimisi.blade.php
+++ resources/views/visimisi.blade.php
@@ -10,9 +10,9 @@
     <div>
         <h2 class="text-navy font-weight-bold text-center">Misi</h2>
         <ol class="text-orange font-weight-bold">
-            <li>Dalam melaksanakan semua kegiatan peduli sosial, anggota Bikers Brotherhood 1% MC selalu dalam semangat dan menjungjung tinggi
+            <li>Dalam melaksanakan semua kegiatan peduli sosial, anggota Mio Riders selalu dalam semangat dan menjungjung tinggi
                 5 Azas (Brotherhood, Loyal, Respect, Honor, Pride).</li>
-            <li>Menyelenggarakan kegiatan sosial kreatif di setiap check point BB 1% MC seluruh Indonesia berbasis penambahan ilmu dan
+            <li>Menyelenggarakan kegiatan sosial kreatif di setiap check point Mio Riders seluruh Indonesia berbasis penambahan ilmu dan
                 pemanfaatan waktu luang anak sekolah dan putus sekolah dari usia PAUD hingga SMA/SMK sebagai pusat kegiatan.</li>
             <li>Membangun kerjasama antar organisasi sosial/komunitas /CSR perusahaan dan unsur masyarakat lain yang memiliki tujuan sama.</li>
             <li>Mewujudkan generasi muda yang peduli, mandiri, kreatif dan inovatif melalui komunitas yang berdaya dan bermanfaat.</li>
